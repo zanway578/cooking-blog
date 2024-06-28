8dd29ebd4d43adeafe33014add66430e68a281b4
Zachary Anway <zanway@ariacommunications.com>
2024-06-28 15:50:29 -0600

    misc: basic layout of nutrition info compiler

---
+++ CookingBlog.Web/Lib/NutritionInformationCompiler.cs
@@ -0,0 +1,22 @@
+﻿using CookingBlog.Database;
+
+namespace CookingBlog.Web.Lib
+{
+    public class NutritionInformationCompiler(Guid recipeId, CookingBlogContext ctx)
+    {
+        private readonly Guid _recipeId = recipeId;
+        private readonly CookingBlogContext _ctx = ctx;
+
+        // TODO: return actual data type
+        public object? CompileNutritionFacts()
+        {
+            /* Algorithm:
+             * 1. Verify all ingredients have a nutrient list - if not, we can't compute.
+             * 2. Produce condensed nutrient list using SQL view.
+             * 3. Pull out important macro nutrient and return as model.
+             */
+
+            return null;
+        }
+    }
+}
