9834df86ca1c8761858fb8e2afbc7c096193c6db
Zachary Anway <zanway@ariacommunications.com>
2024-07-02 09:18:10 -0600

    feat: load copyright via app settings

--- CookingBlog.Web/Models/CookingBlogSettings.cs
+++ CookingBlog.Web/Models/CookingBlogSettings.cs
@@ -5,5 +5,7 @@
         public string ConnectionString { get; set; } = null!;
 
         public string FdcKey { get; set; } = null!;
+
+        public string SiteName { get;set; } = null!;
     }
 }
